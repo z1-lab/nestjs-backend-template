525b6c326d7684866f6983e525ec387769e86517
Horus Lugo <horusgoul@gmail.com>
2020-06-15 19:31:56 +0200

    style: fix lint issues

--- src/scalars/pagination-amount.scalar.ts
+++ src/scalars/pagination-amount.scalar.ts
@@ -7,7 +7,7 @@ const name = 'PaginationAmount';
 @Scalar(name)
 export class PaginationAmountScalar extends GraphQLScalar() {
   protected scalar = createIntScalar({
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     name,
     minimum: 1,
